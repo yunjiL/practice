a8bdc46b252185db85e567a0e2df66e323c5dab5
yunjiL <luj2868@gmail.com>
2023-04-06 04:13:28 +0900

    day1

--- p18185.cpp
+++ p18185.cpp
@@ -1,10 +1,47 @@
 #include <iostream>
+using namespace std;
 
-void main()
+int n;
+int a[100001];
+int ans = 0;
+
+int main()
 {
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
 
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i+1]>a[i+2])
+        {
+            int two = min(a[i], a[i+1]-a[i+2]);
+            ans += 5 * two;
+            a[i] -= two;
+            a[i+1] -= two;
 
+            int three = min(a[i], min(a[i+1], a[i+2]));
+            ans += 7 * three;
+            a[i] -= three;
+            a[i+1] -= three;
+            a[i+2] -= three;
+        }
+        else
+        {
+            int three = min(a[i], min(a[i+1], a[i+2]));
+            ans += 7 * three;
+            a[i] -= three;
+            a[i+1] -= three;
+            a[i+2] -= three;
 
+            int two = min(a[i], a[i+1]);
+            ans += 5 * two;
+            a[i] -= two;
+            a[i+1] -= two;
+        }
+        ans += 3 * a[i];
+    }
 
-
+    cout << ans << endl;
+    return 0;
 }
---
+++ p3109.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+//dfs 알고리즘 사용하여 풀이
+int main()
+{
+
+
+    return 0;
+}
